547874567b210022a069754fef561bfcbcef9bbe
Sidharth Juyal <sidharth.juyal@escaladagroup.com>
2015-05-25 23:25:43 +0530

    Create TMReachability.framework target

---
+++ TMReachability/TMReachability/TMReachability.h
@@ -0,0 +1,19 @@
+//
+//  TMReachability.h
+//  TMReachability
+//
+//  Created by Sid on 25/05/15.
+//  Copyright (c) 2015 Picnic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMReachability.
+FOUNDATION_EXPORT double TMReachabilityVersionNumber;
+
+//! Project version string for TMReachability.
+FOUNDATION_EXPORT const unsigned char TMReachabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMReachability/PublicHeader.h>
+
+
